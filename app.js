9a7523f7f33f3cf916c508f285b2779f9fc8a214
Thesaint00221 <sintobinglenn@gmail.com>
2026-07-08 08:55:52 +0200

    versie 2

--- app.js
+++ app.js
@@ -1,132 +1,27 @@
 let platen = [];
 
-const basis = "./";
+async function laadPlaten() {
 
-const cards = document.getElementById("cards");
-const detail = document.getElementById("detail");
-const detailTitle = document.getElementById("detailTitle");
-const detailCode = document.getElementById("detailCode");
-const detailTable = document.getElementById("detailTable");
-
-
-document
-    .getElementById("backButton")
-    .onclick = () => detail.classList.add("hidden");
-
-
-
-async function start(){
-
-    const response = await fetch(basis + "data.json");
-
+    const response = await fetch("platen.json");
     platen = await response.json();
 
-    render(platen);
-
+    console.log(platen);
 }
 
-
-
-function render(lijst){
-
-    cards.innerHTML = "";
-
-    lijst.forEach(plaat => {
-
-        const card = document.createElement("div");
-
-        card.className = "card";
-
-
-        card.innerHTML = `
-        <div class="cardBody">
-
-            <h2>${plaat.naam}</h2>
-
-            <div class="code">
-                ${plaat.code}
-            </div>
-
-            <div class="leverancier">
-                ${plaat.leverancier}
-            </div>
-
-        </div>
-        `;
-
-
-        card.onclick = () => openDetail(plaat);
-
-
-        cards.appendChild(card);
-
-    });
-
-}
-
-
-
-function openDetail(plaat){
-
-    detail.classList.remove("hidden");
-
-
-    detailTitle.textContent = plaat.naam;
-
-    detailCode.textContent = "Referentie " + plaat.code;
-
-
-    detailTable.innerHTML = `
-
-    <tr>
-        <td>Leverancier</td>
-        <td>${plaat.leverancier}</td>
-    </tr>
-
-    <tr>
-        <td>Referentie</td>
-        <td>${plaat.info?.Referentie ?? ""}</td>
-    </tr>
-
-    <tr>
-        <td>Kleur</td>
-        <td>${plaat.info?.Kleur ?? ""}</td>
-    </tr>
-
-    <tr>
-        <td>Kleurnummer</td>
-        <td>${plaat.info?.Kleurnummer ?? ""}</td>
-    </tr>
-
-    `;
-
-}
-
-
-
+laadPlaten();
 
 document
 .getElementById("search")
-.addEventListener("input", function(){
+.addEventListener("input", zoeken);
 
-    const zoek = this.value.toLowerCase();
+function zoeken(){
 
+    const zoekterm =
+        document
+        .getElementById("search")
+        .value
+        .toLowerCase();
 
-    const resultaat = platen.filter(plaat =>
+    console.log("Zoeken naar:", zoekterm);
 
-        plaat.naam.toLowerCase().includes(zoek) ||
-
-        plaat.code.toLowerCase().includes(zoek) ||
-
-        plaat.leverancier.toLowerCase().includes(zoek)
-
-    );
-
-
-    render(resultaat);
-
-});
-
-
-
-start();
+}
