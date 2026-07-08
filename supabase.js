c19313f51489ceea4a4cf2a6bc63b993fe65a423
Thesaint00221 <sintobinglenn@gmail.com>
2026-07-08 13:14:48 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://tiuxjwaovmchswunwouz.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_uyEOCnVUSLTO5PDFn8tepw_SdN7hHQ6";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
