9e576597810fca71bb7c8af2448a23600701ea78
Thesaint00221 <sintobinglenn@gmail.com>
2026-07-13 06:24:55 +0200

    Update opschonen.js

--- js/opschonen.js
+++ js/opschonen.js
@@ -2,6 +2,69 @@
 // OPSCHONEN STORAGE
 // ============================================
 
+async function haalAlleBestanden(map = "") {
+
+    const { data, error } =
+        await supabaseClient
+            .storage
+            .from("plaatfotos")
+            .list(
+                map,
+                {
+                    limit:1000
+                }
+            );
+
+
+    if(error){
+
+        console.error(error);
+        return [];
+
+    }
+
+
+    let bestanden = [];
+
+
+    for(const item of data){
+
+
+        const pad =
+            map
+            ? `${map}/${item.name}`
+            : item.name;
+
+
+
+        // map herkennen
+        if(!item.metadata){
+
+            const sub =
+                await haalAlleBestanden(pad);
+
+            bestanden.push(
+                ...sub
+            );
+
+        }
+        else {
+
+            bestanden.push(pad);
+
+        }
+
+    }
+
+
+    return bestanden;
+
+}
+
+
+
+
+
 document
 .getElementById("opschonenButton")
 ?.addEventListener(
@@ -9,41 +72,19 @@ document
 async()=>{
 
 
-console.log("Opschonen gestart");
-
-
-const { data: bestanden, error } =
-await supabaseClient
-.storage
-.from("plaatfotos")
-.list(
-"",
-{
-limit:1000
-}
+console.log(
+"Opschonen gestart"
 );
 
 
 
-if(error){
-
-console.error(
-"Fout bij ophalen bestanden:",
-error
-);
-
-alert(
-"Bestanden ophalen mislukt"
-);
-
-return;
-
-}
+const bestanden =
+    await haalAlleBestanden();
 
 
 
 console.log(
-"Bestanden in bucket:",
+"Alle bestanden:",
 bestanden
 );
 
@@ -51,8 +92,9 @@ bestanden
 
 alert(
 bestanden.length +
-" bestanden gevonden. Kijk in F12 console."
+" bestanden gevonden"
 );
+
 
 
 });
