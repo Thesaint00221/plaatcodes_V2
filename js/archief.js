5c871bf40d234f4913c5424a28f7d2bd85fc5637
Thesaint00221 <sintobinglenn@gmail.com>
2026-07-14 13:00:08 +0200

    Update archief.js

--- js/archief.js
+++ js/archief.js
@@ -17,3 +17,83 @@ document
     document.getElementById("resultaten").style.display="";
 
 });
+async function laadArchief(){
+
+    document.getElementById("resultaten").style.display="none";
+
+    archiefPagina.classList.remove("hidden");
+
+    const {data,error} =
+        await supabaseClient
+            .from("platen")
+            .select("*")
+            .eq("gearchiveerd",true)
+            .order("naam");
+
+    if(error){
+
+        console.error(error);
+
+        return;
+
+    }
+
+    archiefResultaten.innerHTML="";
+
+    data.forEach(plaat=>{
+
+        archiefResultaten.innerHTML+=`
+
+<div class="kaart modern-kaart">
+
+    <div class="kaartBody">
+
+        <div class="kaartTitel">
+
+            ${plaat.naam}
+
+        </div>
+
+        <div class="kaartCode">
+
+            ${plaat.code}
+
+        </div>
+
+        <button
+            onclick="terugActief('${plaat.code}')">
+
+            ♻️ Terugzetten
+
+        </button>
+
+    </div>
+
+</div>
+
+`;
+
+    });
+
+}
+async function terugActief(code){
+
+    const {error} =
+        await supabaseClient
+            .from("platen")
+            .update({
+                gearchiveerd:false
+            })
+            .eq("code",code);
+
+    if(error){
+
+        alert("Terugzetten mislukt");
+
+        return;
+
+    }
+
+    laadArchief();
+
+}
